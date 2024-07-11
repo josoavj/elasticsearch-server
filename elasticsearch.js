3c2fcd579968ce90678bddfd1ce75420d97394b0
= <76913187+Josoa886@users.noreply.github.com>
2024-07-11 15:59:34 +0300

    refractor: test

--- elasticsearch.js
+++ elasticsearch.js
@@ -1,14 +1,8 @@
 const { Client } = require('@elastic/elasticsearch')
 const client = new Client({
-  node: 'https://192.168.0.19:9200',
-  auth: {
-    apiKey: 'VWNyTFdKQUJWU21VcHpJUHFaZUI6cDRDSVhvdjBRM08wMUxDamlVVGdKUQ=='
-  },
-  
-  // const client = new Client({node: 'https://stagiaire:Police2405$@192.168.0.19:9200'})
-
-  tls: {
-    ca: fs.readFileSync('/path/to/ca.crt'),
+ node: 'https://stagiaire:Police2405$@192.168.0.19:9200',
+ tls: {
+    ca: fs.readFileSync('/path/to/ca.pem'),
     rejectUnauthorized: false
   }
 })
