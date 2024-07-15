448c92d0654ce2d8ad2035bfe7ab3b5e11408349
= <76913187+Josoa886@users.noreply.github.com>
2024-07-15 11:17:33 +0300

    refactor: Adding the http_ca certificate

--- elasticsearch.js
+++ elasticsearch.js
@@ -2,7 +2,7 @@ const { Client } = require('@elastic/elasticsearch')
 const client = new Client({
  node: 'https://stagiaire:Police2405$@192.168.0.19:9200',
  tls: {
-    ca: fs.readFileSync('/path/to/ca.pem'),
+    ca: fs.readFileSync('/assets/http_ca.crt'),
     rejectUnauthorized: false
   }
 })
