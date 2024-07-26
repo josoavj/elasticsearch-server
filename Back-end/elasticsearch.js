ad753eca28d6b1b2476b7366bd5d1abf9a68d862
josoavj <josoavonjiniaina13@gmail.com>
2024-07-26 15:11:16 +0300

    refactor: Test d'accès aux données du serveur elasticsearch

--- Back-end/elasticsearch.js
+++ Back-end/elasticsearch.js
@@ -91,11 +91,37 @@ async function readElasticIndex() {
         }
       }
     })
-    console.log('Data:', index);
+    console.log('Données', index);
   } catch (error){
     console.error('Error reading data:', error)
   }
 }
+
+async function getLastFiveDocuments(index) {
+  try {
+    const { body } = await client.search({
+      index: index,
+      body: {
+        query: {
+          match_all: {}
+        },
+        sort: [
+          { "_id": { "order": "desc" } }
+        ],
+        size: 5
+      }
+    });
+
+    const hits = body.hits.hits;
+    hits.forEach((hit, index) => {
+      console.log(`Document ${index + 1}:`, hit._source);
+    });
+  } catch (error) {
+    console.error('Erreur lors de la récupération des documents:', error);
+  }
+}
+
+
 
 // Express Server
 app.get('/', (req, res) => {
@@ -110,7 +136,9 @@ app.listen(port, () => {
 // Main basic checking functions
 checkClusterHealth();
 checkNodeHealth();
+checkClientInfo()
 readElasticIndex();
+getLastFiveDocuments('filebeat-8.14.3');
 
 
 
