896d95cb3990c6f0c958136718af79aba70ad92a
josoavj <josoavonjiniaina13@gmail.com>
2024-08-01 08:18:20 +0300

    refactor: Reconfiguration du serveur

--- Back-end/elasticsearch.js
+++ Back-end/elasticsearch.js
@@ -23,88 +23,145 @@ const client = new Client({
    * }
    *
    */
+  auth: {
+    apiKey: "OecGCKrCR4q5q1QHvg0tsw",
+  },
   tls: {
     ca: fs.readFileSync("./assets/http_ca.crt"),
     rejectUnauthorized: false,
   },
 });
 
-async function checkClientInfo() {
-  try {
-    const info = await client.info();
-    console.log("Client Info:", info);
-  } catch (error) {
-    console.error("Error checking the client info:", error);
-  }
-}
+// async function checkClientInfo() {
+//   try {
+//     const info = await client.info();
+//     console.log("Client Info:", info);
+//   } catch (error) {
+//     console.error("Error checking the client info:", error);
+//   }
+// }
 
-// Vérification des informations du cluster
-async function checkClusterHealth() {
-  try {
-    const health = await client.cluster.health();
-    console.log("Cluster Health:", health);
-  } catch (error) {
-    console.error("Error fetching cluster health:", error);
-  }
-}
+// // Vérification des informations du cluster
+// async function checkClusterHealth() {
+//   try {
+//     const health = await client.cluster.health();
+//     console.log("Cluster Health:", health);
+//   } catch (error) {
+//     console.error("Error fetching cluster health:", error);
+//   }
+// }
 
-// Checking the node health
-async function checkNodeHealth() {
-  try {
-    const health = await client.nodes.stats();
-    console.log("Node Health:", health);
-  } catch (error) {
-    console.error("Error fetching node health:", error);
-  }
-}
+// // Checking the node health
+// async function checkNodeHealth() {
+//   try {
+//     const health = await client.nodes.stats();
+//     console.log("Node Health:", health);
+//   } catch
 
-// Read data
-async function readElasticIndex() {
-  try {
-    const index = await client.search({
-      // index: '.ds-filebeat-8.14.3-2024.07.16-000001',
+// async function checkClientInfo() {
+//   try {
+//     const info = await client.info();
+//     console.log("Client Info:", info);
+//   } catch (error) {
+//     console.error("Error checking the client info:", error);
+//   }ction readElasticIndex() {
+//   try {
+//     const index = await client.search({
+//       // index: '.ds-filebeat-8.14.3-2024.07.16-000001',
+//       index: "filebeat-8.14.3",
+
+//       body: { from: 0, size: 10, query: { match_all: {} } },
+//     });
+//     console.log("Données", index);
+//   } catch (error) {
+//     console.error("Error reading data:", error);
+//   }
+// }
+
+// async function getLastFiveDocuments(index) {
+//   try {
+//     const { body } = await client.search({
+//       index: index,
+//       body: {
+//         query: { match_all: {} },
+//         sort: [{ _id: { order: "desc" } }],
+//         size: 5,
+//       },
+//     });
+
+//     const hits = body.hits.hits;
+//     hits.forEach((hit, index) => {
+//       console.log(`Document ${index + 1}:`, hit._source);
+//     });
+//   } catch (error) {
+//     console.error("Erreur lors de la récupération des documents:", error);
+//   }
+// }
+
+// // Express Server
+// app.get("/", (req, res) => {
+//   console.log("The server is running");
+// });
+
+// app.listen(port, () => {
+//   console.log(`The app's listening on port ${port}`);
+// });
+
+// // Main basic checking functions
+// checkClusterHealth();
+// checkNodeHealth();
+// checkClientInfo();
+// readElasticIndex();
+// getLastFiveDocuments("filebeat-8.14.3");
+
+
+const phraseSearch = async (_index, _type, phrase) => {
+  const hits = [];
+  filebeat-8.14.3
+  //const _index = ".ds-filebeat-8.14.3-2024.07.29-000004";
+
+  // only string values are searchable
+  const searchResult = await client
+    .search({
       index: "filebeat-8.14.3",
-
-      body: { from: 0, size: 10, query: { match_all: {} } },
-    });
-    console.log("Données", index);
-  } catch (error) {
-    console.error("Error reading data:", error);
-  }
-}
-
-async function getLastFiveDocuments(index) {
-  try {
-    const { body } = await client.search({
-      index: index,
+      type: document,
       body: {
-        query: { match_all: {} },
-        sort: [{ _id: { order: "desc" } }],
-        size: 5,
+        query: {
+          multi_match: {
+            fields: [
+              'destination',
+              'source',
+            ],
+            query: phrase,
+            type: 'phrase_prefix',
+            
+          },
+        },
+        highlight: {
+          fields: {
+            destination: {},
+            source: {},
+          },
+        },
       },
-    });
-
-    const hits = body.hits.hits;
-    hits.forEach((hit, index) => {
-      console.log(`Document ${index + 1}:`, hit._source);
-    });
-  } catch (error) {
-    console.error("Erreur lors de la récupération des documents:", error);
+    })
+    .catch((e) => console.log('errr', e));
+  if (
+    searchResult &&
+    searchResult.body &&
+    searchResult.body.hits &&
+    searchResult.body.hits.hits &&
+    searchResult.body.hits.hits.length > 0
+  ) {
+    hits.push(...searchResult.body.hits.hits);
   }
-}
 
-// Express Server
-app.get("/", (req, res) => {
-  console.log("The server is running");
-});
+  return {
+    hitsCount: hits.length,
+    hits,
+  };
+};
 
-app.listen(port, () => {
-  console.log(`The app's listening on port ${port}`);
-});
-
-// Main basic checking functions
-checkClusterHealth();
-checkNodeHealth();
-checkClientInfo();
-readElasticIndex();
-getLastFiveDocuments("filebeat-8.14.3");
+module.exports = {
+  phraseSearch
+};
--- Back-end/testElasticsearch.js
+++ Back-end/testElasticsearch.js
@@ -1,4 +1,7 @@
 const fs = require("fs");
+const express = require("express");
+const bodyParser = require('body-parser');
+const app = express();
 
 /**
  * Vérification de la connexion à Elasticsearch
@@ -15,8 +18,35 @@ const client = new Client({
     rejectUnauthorized: false,
   },
 });
-
 client.ping()
     .then(() => console.log('Connected to Elasticsearch'))
     .catch(err => console.error('Error connecting to Elasticsearch', err));
     module.exports = client;
+
+// // Test d'accès aux données dans le serveur elasticsearch
+// fetch("https://192.168.0.19:9200/filebeat-8.14.3/_search")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data))
+//   .catch((error) => console.error("Error:", error));
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// searching on query
+app.get('/search/:index/:type', async (req, res) => {
+  const { phraseSearch } = require('./SearchEngine');
+  const data = await phraseSearch(req.params.index, req.params.type, req.query.q);
+  res.json(data);
+});
+
+
+// Test si le serveur est fonctionnel 
+app.listen(3000, () => console.log('server running at 3000'));
+
+
+
+
