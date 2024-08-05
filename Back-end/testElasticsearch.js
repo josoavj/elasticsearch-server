92e4e23ec5f30bee74566002f3d80ab9691a4cff
Restyled.io <commits@restyled.io>
2024-08-05 05:58:45 +0000

    Restyled by clang-format

--- Back-end/testElasticsearch.js
+++ Back-end/testElasticsearch.js
@@ -5,34 +5,30 @@ const app = express();
 const axios = require('axios');
 const https = require('https');
 
-
-// Desactiver la connexion SSL 
-const agent = new https.Agent({  
-  rejectUnauthorized: false
-});
-
+// Desactiver la connexion SSL
+const agent = new https.Agent({rejectUnauthorized : false});
 
 /**
  * Vérification de la connexion à Elasticsearch
  * Mode de connexion
  * > Via une authentification basique
- * > Plus le certificat de sécurité http.ca géneré automatiquement par elasticsearch
+ * > Plus le certificat de sécurité http.ca géneré automatiquement par
+ * elasticsearch
  */
-const { Client, BaseConnection } = require("@elastic/elasticsearch");
-const { ALL } = require("dns");
+const {Client, BaseConnection} = require("@elastic/elasticsearch");
+const {ALL} = require("dns");
 const client = new Client({
-  node: "https://stagiaire:Police2405$@192.168.0.19:9200",
-  tls: {
-    ca: fs.readFileSync("./assets/http_ca.crt"),
-    rejectUnauthorized: false,
+  node : "https://stagiaire:Police2405$@192.168.0.19:9200",
+  tls : {
+    ca : fs.readFileSync("./assets/http_ca.crt"),
+    rejectUnauthorized : false,
   },
 });
-
 
 client.ping()
     .then(() => console.log('Connected to Elasticsearch'))
     .catch(err => console.error('Error connecting to Elasticsearch', err));
-    module.exports = client;
+module.exports = client;
 
 // // Test d'accès aux données dans le serveur elasticsearch
 // fetch("https://192.168.0.19:9200/filebeat-8.14.3/_search")
@@ -41,37 +37,30 @@ client.ping()
 //   .catch((error) => console.error("Error:", error));
 
 app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+app.use(bodyParser.urlencoded({
+  extended : true,
+}));
 
 // searching on query
 app.get('/search/:index/:type', async (req, res) => {
-  const { phraseSearch } = require('./SearchEngine');
-  const data = await phraseSearch(req.params.index, req.params.type, req.query.q);
+  const {phraseSearch} = require('./SearchEngine');
+  const data =
+      await phraseSearch(req.params.index, req.params.type, req.query.q);
   res.json(data);
 });
 
-
-// Test si le serveur est fonctionnel 
+// Test si le serveur est fonctionnel
 app.listen(3000, () => console.log('server running at 3000'));
 
 // Utilisation de AXIOS
-axios.get('https://192.168.0.19:9200/filebeat-8.14.3/_search', { 
-  httpsAgent: agent,
-  auth: {
-    username: 'stagiaire',
-    password: 'Police2405$'},
-  tls: {
-    ca: fs.readFileSync("./assets/http_ca.crt"),
-    rejectUnauthorized: false,
-  },
- })
-    .then(response => {
-        console.log(response.data);
+axios
+    .get('https://192.168.0.19:9200/filebeat-8.14.3/_search', {
+      httpsAgent : agent,
+      auth : {username : 'stagiaire', password : 'Police2405$'},
+      tls : {
+        ca : fs.readFileSync("./assets/http_ca.crt"),
+        rejectUnauthorized : false,
+      },
     })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+    .then(response => { console.log(response.data); })
+    .catch(error => { console.error('Error:', error); });
