579efef7d37e9aab1b0635e7068e623cc320b10e
Restyled.io <commits@restyled.io>
2024-08-06 18:41:24 +0000

    Restyled by clang-format

--- syslogs/filterTest.js
+++ syslogs/filterTest.js
@@ -1,25 +1,23 @@
-const { error } = require('console');
+const {error} = require('console');
 const SyslogFilterService = require('./syslogFilter/syslogFilterService');
 const fs = require('fs');
 
+fs.readFile(__dirname + '/syslog-content.json', 'utf8', (error, data) => {
+  if (error) {
+    console.log("error reading file", error);
+    return
+  }
+  try {
+    const syslogFromJson = JSON.parse(data);
+    let syslogFilter = new SyslogFilterService(syslogFromJson);
+    let syslogDto = syslogFilter.filterSyslog();
 
-
-fs.readFile(__dirname+'/syslog-content.json', 'utf8',(error, data)=>{
-    if(error){
-        console.log("error reading file",error);
-        return
-    }
-    try{
-        const syslogFromJson = JSON.parse(data);
-        let syslogFilter = new SyslogFilterService(syslogFromJson);
-        let syslogDto = syslogFilter.filterSyslog(); 
-
-        syslogDto.showDetails();
-    }catch (err) {
-        console.error('Error reading or parsing the file:', err);
-    }
+    syslogDto.showDetails();
+  } catch (err) {
+    console.error('Error reading or parsing the file:', err);
+  }
 });
 
-
-//instaciena le SyslogFilterService io de ny param le object bedebe am syslog
-//aveo atsona io syslogFilter io de manome anle SyslogDto io de le SyslogDto le efa vita filtre
+// instaciena le SyslogFilterService io de ny param le object bedebe am syslog
+// aveo atsona io syslogFilter io de manome anle SyslogDto io de le SyslogDto le
+// efa vita filtre
