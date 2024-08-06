da6022db1dae0be94afa342a65e52ce1f42a209e
josoavj <josoavonjiniaina13@gmail.com>
2024-08-06 21:25:21 +0300

    refactor: Updating the code structure to a cleaner version

--- syslogs/filterTest.js
+++ syslogs/filterTest.js
@@ -1,26 +1,24 @@
+const { error } = require('console');
 const SyslogFilterService = require('./syslogFilter/syslogFilterService');
-const fs = require('fs').promises;
+const fs = require('fs');
 
 
 
-async function readJsonFile() {
-    try {
-        //
-        const data = await fs.readFile('syslog-content.json', 'utf8');
-        const jsonObject = JSON.parse(data);
-
-        // jsonObject le am param io le object am syslog
-        let syslogFilter = new SyslogFilterService(jsonObject);
+fs.readFile(__dirname+'/syslog-content.json', 'utf8',(error, data)=>{
+    if(error){
+        console.log("error reading file",error);
+        return
+    }
+    try{
+        const syslogFromJson = JSON.parse(data);
+        let syslogFilter = new SyslogFilterService(syslogFromJson);
         let syslogDto = syslogFilter.filterSyslog(); 
 
         syslogDto.showDetails();
-
-    } catch (err) {
+    }catch (err) {
         console.error('Error reading or parsing the file:', err);
     }
-}
-
-readJsonFile();
+});
 
 
 //instaciena le SyslogFilterService io de ny param le object bedebe am syslog
