23272b0dd488528d0e4efb69e93e54c018a67a6e
Restyled.io <commits@restyled.io>
2024-08-06 14:22:13 +0000

    Restyled by clang-format

--- syslogs/syslogFilter/ajustedSyslog.js
+++ syslogs/syslogFilter/ajustedSyslog.js
@@ -1,14 +1,13 @@
-class AjustedSyslog{
-    constructor(object){
-        this.source = object._source.source;
-        this.destination = object._source.destination;
-        this.network = object._source.network;
-        this.fortinet = object._source.fortinet.firewall;
-        this.rules = object._source.rule;
-    }
+class AjustedSyslog {
+  constructor(object) {
+    this.source = object._source.source;
+    this.destination = object._source.destination;
+    this.network = object._source.network;
+    this.fortinet = object._source.fortinet.firewall;
+    this.rules = object._source.rule;
+  }
 }
 
 module.exports = AjustedSyslog;
 // export {AjustedSyslog}
 // export default AjustedSyslog
-
