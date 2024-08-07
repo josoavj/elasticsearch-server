6fb3d9bcb93245f4639a114f096de6d24566010e
Restyled.io <commits@restyled.io>
2024-08-07 04:55:05 +0000

    Restyled by clang-format

--- syslogs/syslogFilter/syslogDto.js
+++ syslogs/syslogFilter/syslogDto.js
@@ -1,26 +1,26 @@
-class SyslogDto{
-    ipSource = new String();
-    portSource = new Int8Array();
-    bytesSource = new Int16Array();
-    bytesNetwork = new Int16Array();
-    ruleName = new String();
-    ipDestination = new String();
-    portDestination = new Int8Array();
-    addressDestination = new String();
-    bytesDestination = new Int16Array();
-    destinationOrgName = new String();
-    fortinetFirewallDestinetsvc = new String();
-    fortinetFirewallvwlqualitySeqNum = new Int8Array();
-    fortinetFirewallvwlqualitySeqPort = new Int8Array();
-    relatedIps = new Object();
+class SyslogDto {
+  ipSource = new String();
+  portSource = new Int8Array();
+  bytesSource = new Int16Array();
+  bytesNetwork = new Int16Array();
+  ruleName = new String();
+  ipDestination = new String();
+  portDestination = new Int8Array();
+  addressDestination = new String();
+  bytesDestination = new Int16Array();
+  destinationOrgName = new String();
+  fortinetFirewallDestinetsvc = new String();
+  fortinetFirewallvwlqualitySeqNum = new Int8Array();
+  fortinetFirewallvwlqualitySeqPort = new Int8Array();
+  relatedIps = new Object();
 
-    showDetails(){
-        for(let key in this){
-            if(key != "relatedIps"){
-            console.log(key +" : "+this[key])
-        }
-        }
+  showDetails() {
+    for (let key in this) {
+      if (key != "relatedIps") {
+        console.log(key + " : " + this[key])
+      }
     }
+  }
 }
 
 module.exports = SyslogDto;
